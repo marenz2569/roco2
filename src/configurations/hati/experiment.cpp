2afe1cb90a122e1cb24037fda6edc2640a69bdf8
Markus Schmidl <markus.schmidl@mailbox.tu-dresden.de>
2025-01-22 22:24:20 +0100

    experiment/hati: add firestarter kernel

--- src/configurations/hati/experiment.cpp
+++ src/configurations/hati/experiment.cpp
@@ -16,6 +16,7 @@
 #include <roco2/kernels/addpd.hpp>
 #include <roco2/kernels/busy_wait.hpp>
 #include <roco2/kernels/compute.hpp>
+#include <roco2/kernels/firestarter.hpp>
 #include <roco2/kernels/high_low.hpp>
 #include <roco2/kernels/idle.hpp>
 #include <roco2/kernels/matmul.hpp>
@@ -45,6 +46,7 @@ void run_experiments(roco2::chrono::time_point starting_point, bool eta_only)
     roco2::kernels::memory_write<> mem_wrt;
     roco2::kernels::sqrt squareroot;
     roco2::kernels::matmul mm;
+    roco2::kernels::firestarter fs;
     roco2::kernels::idle idle;
     roco2::kernels::mulpd mulpd;
     roco2::kernels::addpd addpd;
@@ -117,7 +119,7 @@ void run_experiments(roco2::chrono::time_point starting_point, bool eta_only)
             experiment(mulpd, on);
             experiment(squareroot, on);
             experiment(mm, on);
-            // experiment(fs, on);
+            experiment(fs, on);
         }
 
         // }
